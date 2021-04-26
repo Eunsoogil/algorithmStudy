0f65f7c290e7cac5a179fc4677c546b9481f6f1d
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-04-26 17:55:05 +0900

    Update binarySearchTrees.js

--- dataStructure/binarySearchTrees.js
+++ dataStructure/binarySearchTrees.js
@@ -27,6 +27,10 @@ binary search trees
 자식들이 정렬되어 있음
 왼쪽은 부모보다 작고 오른쪽은 부모보다 크다
 
+     10
+  5       13
+2   7   11  16
+
 trees > binary trees > binary search trees
 
 */
@@ -45,6 +49,80 @@ class BinarySearchTree {
     }
 
     insert(val){
-    	
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }    	
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+/*
+big O notation
+insertion - O(logN)
+searching - O(logN)
+
+하지만 노드가 한쪽으로 늘어날수록 느려질 수 있다
+
+예시
+
+     3
+       17
+          19
+             32
+                34
+                   63
+                      86
+                         91
+
+위 예시는 BST가 맞지만 상당히 느려질 수 있다
+따라서 이미 정렬된 값들을 binary search tree에 사용하기에는 적합하지 않다
+*/
